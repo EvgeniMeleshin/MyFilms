1b6f93dd5064b7d09ea09089728d7c06ff8ed30d
Evgeni Meleshin <evgenimeleshin@icloud.com>
2022-10-17 21:22:56 +0300

    Initial Commit

---
+++ MyFilms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFilms
+//
+//  Created by Evgeni Meleshin on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
